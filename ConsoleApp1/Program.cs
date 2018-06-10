74d27591a5b51911874df8a7e8fe18781f00eb45
Симона Циева <simona.cieva@students.finki.ukim.mk>
2018-06-10 14:37:10 +0200

    SumUp function is added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,18 @@ namespace Trials_1._0
 {
     class Program
     {
+        static int SumUp(int n) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += 1;
+            }
+            return n;
+        }
+
         static void Main(string[] args)
         {
+            int s = SumUp(10);
+            Console.WriteLine("The sum is: " + s);
 
             Console.Write("Hello");
             Console.WriteLine(" World!");
